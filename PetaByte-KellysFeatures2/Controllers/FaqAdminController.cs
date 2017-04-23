24bc38499167641190fa84a0c103cee3ee769341
selimulated <ahmadnselim@gmail.com>
2017-04-23 17:18:20 -0400

    FaqAdmin

---
+++ PetaByte-KellysFeatures2/Controllers/FaqAdminController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetaByte_KellysFeatures2.Models;
+
+namespace PetaByte_KellysFeatures2.Controllers
+{
+    public class FaqAdminController : Controller
+    {
+        private PetaByteContext db = new PetaByteContext();
+
+        // GET: FaqAdmin
+        public ActionResult Index()
+        {
+            return View(db.Faqs.ToList());
+        }
+
+        // GET: FaqAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // GET: FaqAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FaqAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,question,answer")] Faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Faqs.Add(faq);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(faq);
+        }
+
+        // GET: FaqAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // POST: FaqAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,question,answer")] Faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faq).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(faq);
+        }
+
+        // GET: FaqAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // POST: FaqAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Faq faq = db.Faqs.Find(id);
+            db.Faqs.Remove(faq);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
