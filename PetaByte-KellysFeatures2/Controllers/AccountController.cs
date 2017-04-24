d0a2770d161296df46de365b09223e090fd024b4
BryanStephens <bryanstephensdevelopment@gmail.com>
2017-04-24 17:06:38 -0400

    Added system.net node in web.config for SMTP

---
+++ PetaByte-KellysFeatures2/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetaByte_KellysFeatures2.Models;
+
+namespace PetaByte_KellysFeatures2.Controllers
+{
+    public class AccountController : Controller
+    {
+        PetaByteContext db = new PetaByteContext();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
